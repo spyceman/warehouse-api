51ce3ae9da4d3e4110162e3dc5401765fc1e4390
Alexander Kravtzov <bijomo67@gmail.com>
2021-11-26 09:37:43 +0000

    add tables

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,29 +1,42 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+// import { PrismaClient } from '@prisma/client';
+// const prisma = new PrismaClient();
 
-export async function main() {
-  await prisma.user.create({
-    data: {
-      email: 'example@gmail.com',
-      encrypted_password: 'abc',
-      organization_id: '123',
-    },
-  });
+// export async function main() {
+//   await prisma.organizations.create({
+//     data: {
+//       name: 'a',
+//       location_id: 1,
+//       created_at: new Date(),
+//       updated_at: new Date(),
+//     },
+//   });
 
-  await prisma.user.create({
-    data: {
-      email: 'abc@yandex.ru',
-      encrypted_password: '1337',
-      organization_id: 'qwerty',
-    },
-  });
-}
+//   await prisma.user.create({
+//     data: {
+//       email: 'example@gmail.com',
+//       encrypted_password: 'abc',
+//       organization_id: 0,
+//       created_at: new Date(),
+//       updated_at: new Date(),
+//     },
+//   });
 
-main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+//   await prisma.user.create({
+//     data: {
+//       email: 'abc@yandex.ru',
+//       encrypted_password: '1337',
+//       organization_id: 0,
+//       created_at: new Date(),
+//       updated_at: new Date(),
+//     },
+//   });
+// }
+
+// main()
+//   .catch((e) => {
+//     console.error(e);
+//     process.exit(1);
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
